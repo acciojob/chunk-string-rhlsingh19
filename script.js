16347adc285f0abb70658afe8e04cda71094ba2c
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-01-03 15:57:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-function stringChop(str, size) {
-  // your code here
-	 if (!str) {
+function chunkString(inputString, chunkLength) {
+    if (!inputString) {
         return [];
     }
 
     const chunks = [];
-    let index = 0;
-
-    while (index < str.length) {
-        chunks.push(str.substr(index, chunkLength));
-        index += chunkLength;
+    for (let i = 0; i < inputString.length; i += chunkLength) {
+        const chunk = inputString.slice(i, i + chunkLength);
+        chunks.push(chunk);
     }
 
     return chunks;
 }
+
+// Examples
+console.log(chunkString("Hello, world!", 5)); // Output: ["Hello", ", wor", "ld!"]
+console.log(chunkString("12345", 2));          // Output: ["12", "34", "5"]
+console.log(chunkString("abc", 5));            // Output: ["abc"]
+console.log(chunkString(null, 3));             // Output: []
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
